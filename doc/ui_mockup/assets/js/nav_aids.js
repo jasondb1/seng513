088a56dd71ca61802d54ebdb7ba57427546c95d6
jasondb1 <jason@teamdeboer.ca>
2019-02-27 23:43:45 -0700

    First mockups complete

--- doc/ui_mockup/assets/js/nav_aids.js
+++ doc/ui_mockup/assets/js/nav_aids.js
@@ -10,11 +10,15 @@ function closeNav() {
 
 function openChat() {
     document.getElementById("content").className = "col-9";
-    document.getElementById("chat").style.visibility = "visible";
+    //document.getElementById("chat").style.visibility = "visible";
+    //document.getElementById("chat").style.display = "block";
+    document.getElementById("chat").style.width = "100%";
 }
 
 //close chat
 function closeChat() {
-    document.getElementById("chat").style.visibility = "hidden";
+    //document.getElementById("chat").style.visibility = "hidden";
     document.getElementById("content").className = "col-12";
+    //document.getElementById("chat").style.display = "none";
+    document.getElementById("chat").style.width = "0";
 }
