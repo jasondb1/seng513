6ced8241868050c0dd697f261eadca76f1cbdca8
lysterjason <jason.lyster@ucalgary.ca>
2019-03-31 17:25:56 -0600

    Header bug fix

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -6,23 +6,19 @@ import { Router, NavigationStart } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'smallFish ERP';
   headerVisible: boolean = false;
 
   constructor(private router: Router) {
     router.events.forEach((event) => {
       if (event instanceof NavigationStart) {
-        if (event['url'] != '/projects' || event['url'] != '/admin' || event['url'] != '/po' || event['url'] != '/invoices') {
+        if (event['url'] == '/login' || event['url'] == '/') { 
           this.headerVisible = false;
         } else {
           this.headerVisible = true;
         }
       }
     });
-  }
-
-  ngOnInit() {
-    this.headerVisible = false;
   }
 }
