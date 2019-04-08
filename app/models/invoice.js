c3a1cde3c7270886619bf986032397ffc152b8eb
Matthew Buhler <matthewbuhler@Matthews-MacBook-Air.local>
2019-04-07 18:02:04 -0600

    Lets Try
    Lets Try

--- app/models/invoice.js
+++ app/models/invoice.js
@@ -1,33 +1,33 @@
 const mongoose=require('mongoose');
+let Schema = mongoose.Schema;
 
-const Invoice = mongoose.model('Invoice', {
 
-    id: {
-        type: Number,
+const invoice = new Schema({
+
+//id field is created by Auto-increment, starts at 1 for all documents put in.
+    description: {
+        minlength: 2,
+        maxlength: 255,
+        type: String,
         required: true
     },
 
-    description: {
-       type: String,
-       required: true
+    dateCreated: {
+        type: Date,
+        required: true,
+        default: Date.now
     },
 
     status: {
         type: String,
-        required: true
+        required: true,
     },
-
-    started: {
-        type: String,
-        required: true
-    },
-
-    notes: {
-        type: String,
-        required: true
-    }
-
 
 });
 
-module.exports = {Invoice};
+
+
+module.exports = mongoose.model("invoice",invoice);
+
+
+
